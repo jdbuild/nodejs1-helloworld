b3bb1af4b7df6a76b9564fa5a5266e471994fea0
Jakob Doppler <jakob.doppler@fhstp.ac.at>
2017-08-21 15:02:02 +0200

    'dsadsa'

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// Load the http module to create an http server.
-var http = require('http');
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
-console.log("hello world2");
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:80/");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
