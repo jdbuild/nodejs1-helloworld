f4ead67cfff1f8b0fc127953295eed3424294bc1
Jakob Doppler <jakob.doppler@fhstp.ac.at>
2017-08-21 14:16:26 +0200

    second push

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ var server = http.createServer(function (request, response) {
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(80);
 
+console.log("hello world");
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:80/");
