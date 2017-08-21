f355adfad8d1032576fe6daa659258efe961a514
Jakob Doppler <jakob.doppler@fhstp.ac.at>
2017-08-21 18:05:01 +0200

    passt

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
+app.use(express.static('public'));
+
 
 
 
 app.get('/', function (req, res) {
   res.send('Hello World2!');
   console.log("hello world2");
+});
+
+
+app.get('/file', function (req, res) {
+        res.sendFile('test.html');
 });
 
 app.listen(app.get('port'), function() {
